2e025e4ba366416f7031983605b77d9beb880df8
LevisKimathi <leviskimathi45@gmail.com>
2020-09-22 01:43:31 +0300

    Cover page is now properly styled Loader is now added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 // Dynamic footer with date
 let date = new Date();
 let year = date.getFullYear();
-let footer_content = '<p><a href="https://github.com/LevisKimathi"><i class="fab fa-github"></i></a> <a href="https://www.linkedin.com/in/leviskimathi/"><i class="fab fa-linkedin"></i></a></p>' +
+let footer_content = '<a href="https://github.com/LevisKimathi"><i class="fab fa-github"></i></a> <a href="https://www.linkedin.com/in/leviskimathi/"><i class="fab fa-linkedin"></i></a>' +
     '<br>' +
     '<p>Copyright © '+year+' Levis Kimathi | All rights reserved</p>';
 $('#footer_content').html(footer_content);
+
 // Add particles
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+//Show flame loading
+$(window).on('load', function(){ $("#loader").fadeOut(500);});
